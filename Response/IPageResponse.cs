7a29a40f9010f3e8b4fcdf02e2a9f96b7d94da6c
LucasC <lucas@mymarketplacebuilder.com>
2024-07-29 21:29:48 +0200

    Page Response Deleted

--- Response/IPageResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿
-namespace Core.API.Request.Response.Response;
-
-public interface IPageResponse<T> where T : class
-{
-    bool HasNextPage { get; set; }
-    IEnumerable<T> Response { get; set; }
-}
--- Response/PageResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿
-namespace Core.API.Request.Response.Response;
-
-public class PageResponse<T> : IPageResponse<T> where T : class
-{
-    public IEnumerable<T> Response { get; set; }
-    public bool HasNextPage { get; set; }
-}
