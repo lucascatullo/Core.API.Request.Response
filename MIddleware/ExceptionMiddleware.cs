ddd79de257e446609446a99b5c665b64d1e344ba
LucasC <lucas@mymarketplacebuilder.com>
2025-04-25 17:22:56 +0200

    added exception middleware

---
+++ MIddleware/ExceptionMiddleware.cs
@@ -0,0 +1,34 @@
+﻿
+using Core.API.Request.Response.Handler;
+using Core.API.Request.Response.Request;
+using Microsoft.AspNetCore.Http;
+using System.Text.Json;
+
+namespace Core.Api.Request.Response.MIddleware;
+
+public class ExceptionMiddleware(RequestDelegate next)
+{
+    private readonly RequestDelegate _next = next;
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            await HandleExceptionAsync(context, ex);
+        }
+    }
+
+    private Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        var response = ExceptionHandler.Handle<BaseResponse>(exception, context.Request, true);
+        context.Response.ContentType = "application/json";
+
+        var result = JsonSerializer.Serialize(response.FormatResponse(context));
+        return context.Response.WriteAsync(result);
+    }
+
+}
