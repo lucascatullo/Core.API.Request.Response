098bad71f0c4038f266e08924c900e9b97b86a0c
LucasC <lucas@mymarketplacebuilder.com>
2025-10-15 12:45:48 +0200

    Librería core.exception eliminada.

--- Request/BasePaginatedRequest.cs
+++ Request/BasePaginatedRequest.cs
@@ -1,6 +1,6 @@
 ﻿namespace Core.API.Request.Response.Request;
 
-public class BasePaginatedRequest : BaseModelRequest, IBasePaginatedRequest
+public class BasePaginatedRequest : IBasePaginatedRequest
 {
     public int PageSize { get; set; } = 500;
     public int PageNum { get; set; } = 1;
